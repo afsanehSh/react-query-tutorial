5035c95bcee1a53bb77ab769b7b9944126ec1b42
a.shamsi <a.shamsi@tosantechno.com>
2023-10-23 10:11:19 +0330

    manage data fetching and show the data view

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,17 +1,41 @@
 import { useQuery } from "react-query";
 import axios from 'axios'
 
+const fetchSuperHeroes = () => {
+    return axios.get('http://localhost:4000/superheroes1')
+}
+
 export const RQSuperHeroesPage = () => {
-    const { isLoading, data } = useQuery('super-heroes', () => {
-        return axios.get('http://localhost:4000/superheroes')
-    })
+    const onSuccess = (data) => {
+        console.log('perfoem side effects after data fetching', data)
+    }
+
+    const onError = (error) => {
+        console.log('perfoem side effects after encountering error', error)
+    }
+
+    const { isLoading, data, isError, error, refetch } = useQuery(
+        'super-heroes',
+        fetchSuperHeroes,
+        {
+            enabled: false,
+            onSuccess,
+            onError
+        }
+    )
+
     if (isLoading) {
         return <h2>Loading ...</h2>
     }
 
+    if (isError) {
+        return <h2>{error.message}</h2>
+    }
+
     return (
         <>
-            <div>RQ Super Hereos</div>
+            <h2>RQ Super Hereos</h2>
+            <button onClick={refetch}>fetch data</button>
             {data?.data.map(hero => {
                 return <div key={hero.name}>{hero.name}</div>
             })
